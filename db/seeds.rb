b2cbd1c72fd0e8dcff296ba8e4d4244b688dcb88
jrshort89 <jrshort89@gmail.com>
2021-02-01 17:01:13 -0500

    more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-# User.destroy_all
+User.destroy_all
 Post.destroy_all
 Comment.destroy_all
 Like.destroy_all
 
 imageURL = 'https://images.unsplash.com/photo-1587502537147-2ba64a62e3d3?ixid=MXwxMjA3fDF8MHxzZWFyY2h8MXx8aGlraW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
 
-1.times do 
     User.create( 
         first_name: Faker::Name.first_name,
         middle_name: Faker::Name.middle_name,
         last_name: Faker::Name.last_name,
-        username: Faker::Esport.player,
-        password: "changeme"
+        username: "user",
+        password: "password"
     )
-end
 30.times do 
     Post.create(
         text: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
